a83d64469854285fd1cf41fa8fe46eb7e918941f
Ascherion <johan.desplats@gmail.com>
2018-03-13 10:01:53 +0100

    jojosavetheworld

--- function.php
+++ function.php
@@ -1,32 +1,26 @@
 <?php
-    function
-        include "ConnectBDD.php";
-
-        //This is the directory where images will be saved
-        $target = "pics";
-        $target = $target . basename( $_FILES['Filename']['name']);
-
-        //This gets all the other information from the form
-        $Filename=$_POST['Filename'];
-        $Description=$_POST['Description'];
-        $pic=($_FILES['Filename']['name']);
-
-
-        // Connects to your Database
-        mysql_connect("localhost", "root", "") or die(mysql_error()) ;
-        mysql_select_db("altabotanikk") or die(mysql_error()) ;
-
-        //Writes the information to the database
-        mysql_query("INSERT INTO picture (Filename,Description)
-        VALUES ('$Filename', '$Description')") ;
-
-        //Writes the Filename to the server
-        if(move_uploaded_file($_FILES['Filename']['tmp_name'], $target)) {
-            //Tells you if its all ok
-            echo "The file ". basename( $_FILES['uploadedfile']['Filename']). " has been uploaded, and your information has been added to the directory";
-        } else {
-            //Gives and error if its not
-            echo "Sorry, there was a problem uploading your file.";
+require_once('bdd/ConnectBDD.php');
+if(isset($_FILES) & !empty($_FILES)){
+    $name = $_FILES['profile']['name'];
+    $size = $_FILES['profile']['size'];
+    $type = $_FILES['profile']['type'];
+    $tmp_name = $_FILES['profile']['tmp_name'];
+}
+$location ="uploads/";
+$maxsize = 7000000;
+if(isset($name) &!empty($name)){
+    if($size <= $maxsize){
+        if(move_uploaded_file($tmp_name, $location.$name)){
+            $sql = "INSERT INTO `data` (name, size, type, location) VALUES ('$name', '$size', '$type', '$location$name')";
+            $res = mysqli_query($connection, $sql);
+            if($res){
+                echo "Uploaded successfully";
+            }
+        }else{
+            echo "Failed to Upload";
         }
+    }else{
+        echo "File should be only 7Mo in size";
     }
+}
 ?>
