1140291fff978e7ae61c3d091ad999ac28414979
Yellow <yellowaformac@gmail.com>
2018-03-19 09:56:28 +0100

    pull css

--- uploads/store.php
+++ uploads/store.php
@@ -57,26 +57,5 @@ function upload($connect){
 
     }
 
-
-    // try{
-    //     //check si le fichier existe
-        // if (file_exists($target_file)) {
-        //     $msg= "Ce nom de fichier est déjà dans la base.";
-        //     return $msg;
-        // }
-    //     // Check file size
-    //     if ($_FILES["fileToUpload"]["size"] > 500000) {
-    //         echo "Sorry, ton fichier est trop lourd.";
-    //     }
-    //     if(move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $target_file)){
-    //         echo "L'image ".  basename( $_FILES['fileToUpload']['name']).
-    //             "a bien été chargé!";
-    //     }
-    //     header("Location: ../index.php?id=".$_SESSION['id']);
-    // }
-    // catch(Exception $e){
-    //         echo "Request failed : " . $e->getMessage();
-    // }
 }
-    // upload($connect);
 ?>
