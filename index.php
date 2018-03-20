0bf451efcdc594a1280c637b02b4d7d2ce1161f5
Yellow <yellowaformac@gmail.com>
2018-03-20 12:07:21 +0100

    push file css

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 ?>
  <!-- Navigation haut de page // A finir, faire entièrement en js ?-->
 <nav id="navigation" class="navbar navbar-expand-lg navbar-light mainNav">
-    <h1 class="navbar-brand mainTitle col-9">File Uploader</h1>
+    <h1 class="navbar-brand mainTitle col-8">File Uploader</h1>
     <button class="navbar-toggler burgerBtn" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -97,7 +97,8 @@
 <div class="col-6 offset-3 fileChooser p-3">
     <form class="text-center" action="uploads/store.php" method="post" enctype="multipart/form-data">
         <div class="col-12 mb-2">
-            <input type="file" name="fileToUpload" id="fileToUpload">
+            <label for="fileToUpload" class="label-file">Parcourir fichiers...</label>
+            <input class="input-file" type="file" name="fileToUpload" id="fileToUpload">
         </div>
         <div class="col-12 mb-2">
             <button type="submit" name="envoiFile" value="Upload Image" class="btn last-btn">Envoyer</button>
